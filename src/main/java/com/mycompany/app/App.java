9557bfb0c8134c8f5e9c137908d317b2bd8de401
dv66 <mostofamonsur9396@gmail.com>
2019-07-04 15:52:36 +0600

    app.java changed

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+	String myString = "this is cokreates";
+        System.out.println(myString );
     }
 }
